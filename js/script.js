2057d562d0701152ef03a87154bd18a72546a391
Nicholas Tsafos <nikotsafos@Nicholass-MacBook-Pro.local>
2018-08-10 16:53:35 -0700

    disable movement after gameover

--- js/script.js
+++ js/script.js
@@ -50,10 +50,12 @@ function preload() {
     game.load.spritesheet('swordguywalk', '../assets/adventurer-run3-sword-Sheet.png', 50, 37);
     game.load.spritesheet('enemy', "../assets/SkeletonWalk.png", 22, 33, 13);
     // game.load.spritesheet('dead', "../assets/dead.png", 64, 29, 12);
-    game.load.spritesheet('attack', '../assets/attack2.png', 43, 32);
+    game.load.spritesheet('attack', '../assets/attack2.png', 44, 32);
 
     game.load.audio('music', '../assets/song.mp3');
     game.load.audio('steps', '../assets/steps.mp3');
+    game.load.audio('pickup', '../assets/pickup.mp3');
+    game.load.audio('sword', '../assets/sword.wav');
 
 }
 
@@ -62,6 +64,8 @@ function create() {
     // var background = game.add.tileSprite(0, 50, 5120, 5120, 'background');
     music = game.add.audio('music');
     steps = game.add.audio('steps', 0.33);
+    pickip = game.add.audio('pickup');
+    sword = game.add.audio('sword');
     music.play();
 
     map = game.add.tilemap('level1');
@@ -71,7 +75,7 @@ function create() {
     wallLayer = map.createLayer('walls');
     map.setCollisionBetween(0, 52, true, wallLayer);
 
-    // backgroundLayer.resizeWorld();
+    backgroundLayer.resizeWorld();
 
 
     // game.world.setBounds(0, 50, 5120, 5120);
@@ -105,33 +109,42 @@ function create() {
 
 
     bow = game.add.sprite(2608, 2640, 'bow');
+    // bow = game.add.sprite(150, 150, 'bow');
     game.physics.arcade.enable(bow);
 
-    apple = game.add.sprite(0, 0), 'apple');
+    apple = game.add.sprite(Math.floor(Math.random() * 1920), Math.floor(Math.random() * 1920), 'apple');
     game.physics.arcade.enable(apple);
 
     cheese = game.add.sprite((180 * 16), (165 * 16), 'cheese');
+    // cheese = game.add.sprite(150, 150, 'cheese');
     game.physics.arcade.enable(cheese);
 
-    boots = game.add.sprite(Math.floor(Math.random() * 1920), Math.floor(Math.random() * 1920), 'boots');
+    boots = game.add.sprite((269 * 16), (123 * 16), 'boots');
+    // boots = game.add.sprite(150, 150, 'boots');
     game.physics.arcade.enable(boots);
 
     cup = game.add.sprite((136 * 16), (135 * 16), 'cup');
+    // cup = game.add.sprite(150, 150, 'cup');
     game.physics.arcade.enable(cup);
 
-    gloves = game.add.sprite(Math.floor(Math.random() * 1920), Math.floor(Math.random() * 1920), 'gloves');
+    gloves = game.add.sprite((225 * 16), (287 * 16), 'gloves');
+    // gloves = game.add.sprite(150, 150, 'gloves');
     game.physics.arcade.enable(gloves);
 
-    key = game.add.sprite(Math.floor(Math.random() * 1920), Math.floor(Math.random() * 1920), 'key');
+    key = game.add.sprite((14 * 16), (123 * 16), 'key');
+    // key = game.add.sprite(150, 150, 'key');
     game.physics.arcade.enable(key);
 
-    necklace = game.add.sprite(Math.floor(Math.random() * 1920), Math.floor(Math.random() * 1920), 'necklace');
+    necklace = game.add.sprite((9 * 16), (224 * 16), 'necklace');
+    // necklace = game.add.sprite(150, 150, 'necklace');
     game.physics.arcade.enable(necklace);
 
-    potion = game.add.sprite(Math.floor(Math.random() * 1920), Math.floor(Math.random() * 1920), 'potion');
+    potion = game.add.sprite((94 * 16), (156 * 16), 'potion');
+    // potion = game.add.sprite(150, 150, 'potion');
     game.physics.arcade.enable(potion);
 
-    ring = game.add.sprite(Math.floor(Math.random() * 1920), Math.floor(Math.random() * 1920), 'ring');
+    ring = game.add.sprite((23 * 16), (57 * 16), 'ring');
+    // ring = game.add.sprite(150, 150, 'ring');
     game.physics.arcade.enable(ring);
 
 
@@ -200,7 +213,8 @@ function update() {
     // swordguywalk.animations.play('walk', 30, true);
   }
   if (game.input.keyboard.isDown(Phaser.Keyboard.SPACEBAR)) {
-    // player.animations.play('attack', 30);
+    sword.play();
+    player.animations.play('attack');
 }
 
   game.physics.arcade.overlap(player, enemy, hurtPlayer);
@@ -241,6 +255,7 @@ function hurtPlayer() {
     console.log(PLAYER_HEALTH);
     playerKill();
     $('#gamelog').prepend('You DIED</br>');
+    enemy.animations.destroy();
   }
 
 }
@@ -251,6 +266,7 @@ function collectApple() {
   apple.kill();
   $('#gamelog').prepend('You ate the poison Apple</br>');
   $('#gamelog').prepend('You DIED</br>');
+  enemy.animations.destroy();
   swal({
   title: 'You ate the poison apple!',
   text: 'Thanks for playing!',
@@ -265,40 +281,31 @@ function collectApple() {
 
 function collectBow() {
   bow.kill();
+  pickip.play();
   $("#items").append("<img src='../assets/bow.png'>");
   $('#gamelog').prepend('Picked up a Bow</br>');
-  score += 1;
-  if (score === 9) {
-    $('#gamelog').prepend('YOU FOUND ALL THE ITEMS! YOU WIN!</br>');
-    game.physics.arcade.disable(player);
-    game.physics.arcade.disable(spawnEnemy);
-  }
+  stuff += 1;
+  checkWin();
 }
 
 function collectBoots() {
   boots.kill();
+  pickip.play();
   PLAYER_SPEED = PLAYER_SPEED * 1.25;
   $("#items").append("<img src='../assets/boots.png'>");
   $('#gamelog').prepend('Picked up Boots. Speed increased 25%</br>');
-  score += 1;
-  if (score === 9) {
-    $('#gamelog').prepend('YOU FOUND ALL THE ITEMS! YOU WIN!</br>');
-    game.physics.arcade.disable(player);
-    game.physics.arcade.disable(spawnEnemy);
-  }
+  stuff += 1;
+  checkWin();
 }
 
 function collectCheese() {
   cheese.kill();
+  pickip.play();
   PLAYER_HEALTH = PLAYER_HEALTH + 25;
   $("#items").append("<img src='../assets/cheese.png'>");
   $('#gamelog').prepend('Picked up some cheese. Health +25</br>');
-  score += 1;
-  if (score === 9) {
-    $('#gamelog').prepend('YOU FOUND ALL THE ITEMS! YOU WIN!</br>');
-    game.physics.arcade.disable(player);
-    game.physics.arcade.disable(spawnEnemy);
-  }
+  stuff += 1;
+  checkWin();
 //   swal({
 //   title: 'You ate some cheese!',
 //   text: '+25hp',
@@ -312,80 +319,72 @@ function collectCheese() {
 
 function collectCup() {
   cup.kill();
+  pickip.play();
   $("#items").append("<img src='../assets/cup.png'>");
   $('#gamelog').prepend('Picked up a Cup</br>');
-  score += 1;
-  if (score === 9) {
-    $('#gamelog').prepend('YOU FOUND ALL THE ITEMS! YOU WIN!</br>');
-    game.physics.arcade.disable(player);
-    game.physics.arcade.disable(spawnEnemy);
-  }
+  stuff += 1;
+  checkWin();
 }
 
 function collectGloves() {
   gloves.kill();
+  pickip.play();
   $("#items").append("<img src='../assets/gloves.png'>");
   $('#gamelog').prepend('Picked up some Gloves</br>');
-  score += 1;
-  if (score === 9) {
-    $('#gamelog').prepend('YOU FOUND ALL THE ITEMS! YOU WIN!</br>');
-    game.physics.arcade.disable(player);
-    game.physics.arcade.disable(spawnEnemy);
-  }
+  stuff += 1;
+  checkWin();
 }
 
 function collectKey() {
   key.kill();
+  pickip.play();
   $("#items").append("<img src='../assets/key.png'>");
   $('#gamelog').prepend('Picked up a Key</br>');
-  score += 1;
-  if (score === 9) {
-    $('#gamelog').prepend('YOU FOUND ALL THE ITEMS! YOU WIN!</br>');
-    game.physics.arcade.disable(player);
-    game.physics.arcade.disable(spawnEnemy);
-  }
+  stuff += 1;
+  checkWin();
 }
 
 function collectNecklace() {
   necklace.kill();
+  pickip.play();
   $("#items").append("<img src='../assets/necklace.png'>");
   $('#gamelog').prepend('Picked up a Necklace</br>');
-  score += 1;
-  if (score === 9) {
-    $('#gamelog').prepend('YOU FOUND ALL THE ITEMS! YOU WIN!</br>');
-    game.physics.arcade.disable(player);
-    game.physics.arcade.disable(spawnEnemy);
-  }
+  stuff += 1;
+  checkWin();
 }
 
 function collectPotion() {
   potion.kill();
+  pickip.play();
   $("#items").append("<img src='../assets/potion.png'>");
   $('#gamelog').prepend('Picked up a Potion</br>');
-  score += 1;
-  if (score === 9) {
-    $('#gamelog').prepend('YOU FOUND ALL THE ITEMS! YOU WIN!</br>');
-    game.physics.arcade.disable(player);
-    game.physics.arcade.disable(spawnEnemy);
-  }
+  stuff += 1;
+  checkWin();
 }
 
 function collectRing() {
   ring.kill();
+  pickip.play();
   $("#items").append("<img src='../assets/ring.png'>");
   $('#gamelog').prepend('Picked up a Ring</br>');
-  score += 1;
-  if (score === 9) {
-    $('#gamelog').prepend('YOU FOUND ALL THE ITEMS! YOU WIN!</br>');
-    game.physics.arcade.disable(player);
-    game.physics.arcade.disable(spawnEnemy);
-  }
+  stuff += 1;
+  checkWin();
 }
 
 function playerKill() {
   // player.animations.play('dead');
   player.kill();
   // location.reload();
+}
+
+function checkWin() {
+  if (stuff === 9) {
+    $('#gamelog').prepend('YOU FOUND ALL THE ITEMS! YOU WIN!</br>');
+    player.body.moves = false;
+    player.animations.destroy();
+    enemy.body.moves = false;
+    enemy.animations.destroy();
+  }
 }
 
 // function spawnEnemy() {
